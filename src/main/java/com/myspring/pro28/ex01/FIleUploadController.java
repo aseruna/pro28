baa7b6361ce3cc55d822902deb8ba12e1eceac62
beenjh6088 <beenjh6088@gmail.com>
2020-12-21 20:52:06 +0900

    pro28 개발
    1. 파일 업로드, 다운로드 개발

---
+++ src/main/java/com/myspring/pro28/ex01/FIleUploadController.java
@@ -0,0 +1,68 @@
+package com.myspring.pro28.ex01;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FIleUploadController {
+	private static final String CURR_IMAGE_REPO_PATH = "C:\\DEV\\spring\\image_repo";
+	
+	@RequestMapping(value = "/form")
+	public String form() {
+		return "uploadForm";
+	}
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/upload")
+	public ModelAndView upload(MultipartHttpServletRequest multipartRequest, HttpServletResponse response) throws Exception{
+		multipartRequest.setCharacterEncoding("utf-8");
+		Map<String, Object> map = new HashMap<String, Object>();
+		Enumeration<Object> enu = multipartRequest.getParameterNames();
+		while(enu.hasMoreElements()) {
+			String name = (String) enu.nextElement();
+			String value = multipartRequest.getParameter(name);
+			map.put(name, value);
+		}
+		List<String> fileList = fileProcess(multipartRequest);
+		map.put("fileList", fileList);
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("map", map);
+		mav.setViewName("result");
+		return mav;
+	}
+	
+	private List<String> fileProcess(MultipartRequest multipartRequest) throws Exception{
+		List<String> fileList = new ArrayList<String>();
+		Iterator<String> fileNames = multipartRequest.getFileNames();
+		while(fileNames.hasNext()) {
+			String fileName = fileNames.next();
+			MultipartFile mFile = multipartRequest.getFile(fileName);
+			String originalFileName = mFile.getOriginalFilename();
+			fileList.add(originalFileName);
+			File file = new File(CURR_IMAGE_REPO_PATH+"\\"+fileName);
+			if(mFile.getSize() != 0) {
+				if(! file.exists()) {
+					if(file.getParentFile().mkdirs()) {
+						file.createNewFile();
+					}
+				}
+				mFile.transferTo(new File(CURR_IMAGE_REPO_PATH+"\\"+originalFileName));
+			}
+		}
+		return fileList;
+	}
+}
