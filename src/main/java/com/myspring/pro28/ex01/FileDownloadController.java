5d274ae9658da242d6c81aeef905bb3b1ada6de6
beenjh6088 <beenjh6088@gmail.com>
2020-12-23 20:45:14 +0900

    pro28 개발
    1. 파일 다운로드시 thumbnail로 구현

--- src/main/java/com/myspring/pro28/ex01/FileDownloadController.java
+++ src/main/java/com/myspring/pro28/ex01/FileDownloadController.java
@@ -10,26 +10,68 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import net.coobird.thumbnailator.Thumbnails;
+
 @Controller
 public class FileDownloadController {
 	private static String CURR_IMAGE_REPO_PATH = "C:\\DEV\\spring\\image_repo";
 	
-	@RequestMapping(value = "/download")
+//	@RequestMapping(value = "/download")
+//	public void download(@RequestParam("imageFileName") String imageFileName, HttpServletResponse response) throws Exception{
+//		OutputStream out = response.getOutputStream();
+//		String downFile = CURR_IMAGE_REPO_PATH+"\\"+imageFileName;
+//		File file = new File(downFile);
+//		
+//		response.setHeader("Cache-Control", "no-cache");
+//		response.addHeader("Content-disposition", "attachment; fileName="+imageFileName);
+//		FileInputStream in = new FileInputStream(file);
+//		byte[] buffer = new byte[1024*8];
+//		while(true) {
+//			int count = in.read(buffer);
+//			if(count == -1) break;
+//			out.write(buffer, 0, count);
+//		}
+//		in.close();
+//		out.close();
+//	}
+	
+//	@RequestMapping("/download")
+//	public void download(@RequestParam("imageFileName") String imageFileName, HttpServletResponse response) throws Exception{
+//		OutputStream out = response.getOutputStream();
+//		String filePath = CURR_IMAGE_REPO_PATH+"\\"+imageFileName;
+//		File image = new File(filePath);
+//		int lastIndex = imageFileName.lastIndexOf(".");
+//		String fileName = imageFileName.substring(0,lastIndex);
+//		File thumbnail = new File(CURR_IMAGE_REPO_PATH+"\\thumbnail\\"+fileName+".png");
+//		if(image.exists()) {
+//			thumbnail.getParentFile().mkdirs();
+//			Thumbnails.of(image).size(50, 50).outputFormat("png").toFile(thumbnail);
+//		}
+//		
+//		FileInputStream in = new FileInputStream(thumbnail);
+//		byte[] buffer = new byte[1024*8];
+//		while(true) {
+//			int count = in.read(buffer);
+//			if(count == -1) break;
+//			out.write(buffer, 0, count);
+//		}
+//		in.close();
+//		out.close();
+//	}
+	
+	@RequestMapping("/download")
 	public void download(@RequestParam("imageFileName") String imageFileName, HttpServletResponse response) throws Exception{
 		OutputStream out = response.getOutputStream();
-		String downFile = CURR_IMAGE_REPO_PATH+"\\"+imageFileName;
-		File file = new File(downFile);
-		
-		response.setHeader("Cache-Control", "no-cache");
-		response.addHeader("Content-disposition", "attachment; fileName="+imageFileName);
-		FileInputStream in = new FileInputStream(file);
-		byte[] buffer = new byte[1024*8];
-		while(true) {
-			int count = in.read(buffer);
-			if(count == -1) break;
-			out.write(buffer, 0, count);
+		String filePath = CURR_IMAGE_REPO_PATH+"\\"+imageFileName;
+		File image = new File(filePath);
+		if(image.exists()) {
+			Thumbnails.of(image).size(50, 50).outputFormat("png").toOutputStream(out);
+		}else {
+			return;
 		}
-		in.close();
+		
+		byte[] buffer = new byte[1024*8];
+		out.write(buffer);
 		out.close();
 	}
 }
