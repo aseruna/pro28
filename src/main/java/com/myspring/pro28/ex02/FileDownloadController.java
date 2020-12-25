fcaf39b5c38dcb01cf4f59dadcccb9886f12550a
beenjh6088 <beenjh6088@gmail.com>
2020-12-25 18:02:23 +0900

    pro28 개발
    1. FileUploadController 세분화
    2. Mail Service 초반부 작성

--- src/main/java/com/myspring/pro28/ex02/FileDownloadController.java
+++ src/main/java/com/myspring/pro28/ex02/FileDownloadController.java
@@ -1,7 +1,6 @@
-package com.myspring.pro28.ex01;
+package com.myspring.pro28.ex02;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletResponse;
@@ -10,28 +9,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@Controller
-@SuppressWarnings("unused")
+import net.coobird.thumbnailator.Thumbnails;
+
+@Controller
 public class FileDownloadController {
 	private static String CURR_IMAGE_REPO_PATH = "C:\\DEV\\spring\\image_repo";
 	
-	@RequestMapping(value = "/download")
+	@RequestMapping("/download")
 	public void download(@RequestParam("imageFileName") String imageFileName, HttpServletResponse response) throws Exception{
 		OutputStream out = response.getOutputStream();
-		String downFile = CURR_IMAGE_REPO_PATH+"\\"+imageFileName;
-		File file = new File(downFile);
-		
-		response.setHeader("Cache-Control", "no-cache");
-		response.addHeader("Content-disposition", "attachment; fileName="+imageFileName);
-		FileInputStream in = new FileInputStream(file);
-		byte[] buffer = new byte[1024*8];
-		while(true) {
-			int count = in.read(buffer);
-			if(count == -1) break;
-			out.write(buffer, 0, count);
+		String filePath = CURR_IMAGE_REPO_PATH+"\\"+imageFileName;
+		File image = new File(filePath);
+		if(image.exists()) {
+			Thumbnails.of(image).size(50, 50).outputFormat("png").toOutputStream(out);
+		}else {
+			return;
 		}
-		in.close();
+		
+		byte[] buffer = new byte[1024*8];
+		out.write(buffer);
 		out.close();
 	}
-	
 }
