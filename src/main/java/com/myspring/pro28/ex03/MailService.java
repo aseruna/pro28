dfe50c9241915430383ab18be053feea4e61fb3f
beenjh6088 <beenjh6088@gmail.com>
2021-01-01 13:03:19 +0900

    pro28 개발
    1. 소스정리

--- src/main/java/com/myspring/pro28/ex03/MailService.java
+++ src/main/java/com/myspring/pro28/ex03/MailService.java
@@ -7,7 +7,6 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
 
 //@Service("mailService")
 public class MailService {
